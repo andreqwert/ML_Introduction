523394352ab0f86c4aece94e63153d80de08f054
Andrey <timebird@yandex.ru>
2019-02-10 02:40:32 +0300

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,38 +1,38 @@
+from keras.datasets import cifar10 
 import numpy as np
 
 
-class NearestNeighbour:
-	def __init__(self):
-		pass
+
+class NearestNeighbor(object):
+    def __init__(self):
+        pass
+
+    def train(self, X, y):
+        """X = N x D where each row is an example. Y is 1-dimensional of size N"""
+        self.Xtr = X
+        self.ytr = y
+
+    def predict(self, X):
+        """X = N x D where each row is an example we wish to predict label for"""
+        num_test = X.shape[0]
+        Ypred = np.zeros(num_test, dtype=ytr.dtype) # lets make sure the output type matches the input type
+
+        # loop over all test rows
+        for i in range(num_test):
+            # find the nearest training image to the i'th test image
+            # using the L1 distance (sum of absolute value differences)
+            distances = np.sum(np.abs(self.Xtr - X[i,:]), axis=1)
+            min_index = np.argmin(distances) # get the index with smallest distance
+            Ypred[i] = self.ytr[min_index] # predict the label of the nearest example
+        return Ypred
 
 
-	def train(self, X, ):
-		"""
-		X is N * D where each row is an example.
-		Y is 1-dimensional of size N.
+(Xtr, ytr), (Xte, Yte) = cifar10.load_data()
+Xtr_rows = Xtr.reshape(Xtr.shape[0], 32*32*3)
+Xte_rows = Xte.reshape(Xte.shape[0], 32*32*3)
 
-		The KNN simply remembers all the trianing data.
-		"""
-		self.Xtr = X
-		self.Ytr = y
-
-
-	def predict(self, X):
-		"""
-		X is N * D where each row is an example we wish to predict label for.
-		Lets make sure that the output type matches the input type
-		"""
-		Ypred = np.zeros(num_test. dtype=self.ytr.dtype)
-
-		# loop over all test rows
-		for i in xrange(num_test):
-			# find te nearest training image to the i'th test image
-			# using the L1 distance (sum of absolute value differences)
-			distances = np.sum(np.abs(self.Xtr - X[i,:]), axis=1)
-			min_index = np.argmin(distances) # get the index with smallest distance
-			Ypred[i] = self.ytr[min_index] # predict the label of the nearest example
-
-		return Ypred
-
-
+nn = NearestNeighbor() # create a Nearest Neighbor classifier class
+nn.train(Xtr_rows, ytr) # train the classifier on the training images and labels
+Yte_predict = nn.predict(Xte_rows) # predict labels on the test images
+print('Accuracy: {}'.format(np.mean(Yte_predict=Yte)))
 
